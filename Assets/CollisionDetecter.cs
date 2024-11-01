3a9a1117779b57f4eb24b3cafefcfbfbc2d23198
seokmin111 <seokmin0111@gmail.com>
2024-11-02 03:45:13 +0900

    배경완

---
+++ Assets/CollisionDetecter.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CollisionDetecter : MonoBehaviour
+{
+    // Reference to the Flame GameObject and its ParticleSystem
+    public GameObject flame;
+    private ParticleSystem flameParticles;
+    public float reductionAmount = 0.1f; // Amount to reduce the emission each time
+    private float currentEmission = 1.0f; // Initial emission rate (normalized)
+
+    void Start()
+    {
+        // Get the ParticleSystem component from the Flame object
+        if (flame != null)
+        {
+            flameParticles = flame.GetComponent<ParticleSystem>();
+        }
+    }
+
+    // Called when another collider enters the trigger collider attached to this object
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the object we collided with has the tag "Square"
+        if (collision.gameObject.CompareTag("Square"))
+        {
+            // Reduce the emission of the flame
+            if (flameParticles != null && currentEmission > 0)
+            {
+                currentEmission -= reductionAmount;
+                currentEmission = Mathf.Clamp(currentEmission, 0, 1); // Clamp to ensure it doesn't go below 0
+
+                // Update the emission rate
+                var emission = flameParticles.emission;
+                emission.rateOverTime = emission.rateOverTime.constant * currentEmission;
+
+                // If the emission reaches 0, disable the flame
+                if (currentEmission <= 0)
+                {
+                    flame.SetActive(false);
+                    Debug.Log("Flame has been completely extinguished!");
+                }
+            }
+        }
+    }
+}
